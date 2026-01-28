c88a5d92e8a0e0e354498cd1f8277e5feb1a468d
Pikiss-personal <164522702+Pikiss-personal@users.noreply.github.com>
2026-01-28 17:19:59 +0900

    Switch Room annotation processing to KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("org.jetbrains.kotlin.kapt")
-}
-
-if (plugins.hasPlugin("org.jetbrains.kotlin.kapt")) {
-    error("KAPT is not supported with Kotlin 2.x in this project. Use KSP instead.")
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -62,7 +58,7 @@ dependencies {
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
-    kapt(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
