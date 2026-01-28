dcc8427357238581848484e3174ca43ad0306dbb
Pikiss-personal <164522702+Pikiss-personal@users.noreply.github.com>
2026-01-28 18:10:08 +0900

    Align FTS entity with content entity

--- app/src/main/java/com/echo/echocalendar/data/local/EventFtsEntity.kt
+++ app/src/main/java/com/echo/echocalendar/data/local/EventFtsEntity.kt
@@ -1,12 +1,13 @@
 package com.echo.echocalendar.data.local
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Fts5
 
-@Fts5
+@Fts5(contentEntity = EventEntity::class)
 @Entity(tableName = "EventFts")
 data class EventFtsEntity(
-    val eventId: String,
+    @ColumnInfo(name = "id") val eventId: String,
     val summary: String,
     val body: String,
     val placeText: String?
