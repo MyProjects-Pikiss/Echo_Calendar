3052cf963b81bffa7e487aad057962c2153a6107
Pikiss-personal <164522702+Pikiss-personal@users.noreply.github.com>
2026-01-30 17:09:01 +0900

    Align toolchain to GPT-recommended versions

--- app/src/main/java/com/echo/echocalendar/data/local/AppDatabase.kt
+++ app/src/main/java/com/echo/echocalendar/data/local/AppDatabase.kt
@@ -12,7 +12,7 @@ import androidx.room.RoomDatabase
         EventFtsEntity::class,
         EventAlarmEntity::class
     ],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
