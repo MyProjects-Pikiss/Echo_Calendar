ea3bc8e148569698a93ebecdec478b93668a048e
Pikiss-personal <164522702+Pikiss-personal@users.noreply.github.com>
2026-02-03 18:14:39 +0900

    Bump compile and target SDK to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.echo.echocalendar"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.echo.echocalendar"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
