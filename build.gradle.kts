6b1a01483fbbd27fd1f6d541da30d87b7e6cab83
Pikiss-personal <164522702+Pikiss-personal@users.noreply.github.com>
2026-02-03 15:22:16 +0900

    Bump build tooling and KSP versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp) apply false
+}
+
+allprojects {
+    configurations.configureEach {
+        resolutionStrategy.force("com.google.devtools.ksp:symbol-processing-api:2.3.4")
+        resolutionStrategy.force("com.google.devtools.ksp:symbol-processing:2.3.4")
+    }
 }
